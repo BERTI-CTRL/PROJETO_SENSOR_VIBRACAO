e5f05c32157a87afc35ba037ba893bec94006a39
Berti <mateusbertib@gmail.com>
2026-06-30 11:40:22 -0300

    hzs e atributos corrigidos

--- sensor/coletor.py
+++ sensor/coletor.py
@@ -19,29 +19,37 @@ class Coletor():
             arquivo="dados_vibracao.csv",
             tamanho_buffer=1000
         )
-        self.hz_medio = 0
-        self.hz_inst = 0
-        self.hz_amostras = 0
-        self.t0 = 0
+        self.numero_amostras = 0
+
+        self.hz_medio = 0 #  valor do hz medio
+        self.hz_inst = 0 # valor do hz instantaneo
+        self.hz_inst_suavizado = 0 # valor do hz instantaneo suavizado
+
+        self.t0 = 0 
         self.t_anterior = 0
-        
-        self.hzs = deque(maxlen=100)
-        
+
+        self.historico_hzs_inst = deque(maxlen=100) # fila usada para calcular o valor hz instantaneo suavizado - > GUARDA OS ÚLTIMOS 100 HZ 
     
     def executar(self):
+
         self.rodando = True
-        self.hz = 0
-        self.amostras = 0
+
+        self.numero_amostras = 0
+
         self.hz_medio = 0
         self.hz_inst = 0
+        self.hz_inst_suavizado = 0
+
+
         self.t0 = time.perf_counter()
         self.t_anterior = self.t0
-        self.hzs.clear()
 
-
-
+        self.historico_hzs_inst.clear()
 
         print("Coletor iniciado")
+
+
+
         
         try:
 
@@ -49,36 +57,63 @@ class Coletor():
                     porta=self.porta,
                    baudrate=self.baudrate
                 ):
-                    self.amostras += 1
+                    self.numero_amostras += 1
 
                     #####################################
                     # Estatísticas da aquisição
                     #####################################
+                    agora = time.perf_counter()
+                    dt = agora - self.t0
 
 
                     #####################################
                     #Hertz médio
                     #####################################
-                    dt = time.perf_counter() - self.t0
-                    self.hz_medio = self.amostras/dt
+                    
+
+                    if dt>0:
+
+                        self.hz_medio = self.numero_amostras/dt
+
+
+
+                    ###################################
+                    #Hz inst
+                    ###################################
+                    dt_inst = agora = self.t_anterior
+                    self.t_anterior = agora
+
+
 
                     #####################################
-                    #Hertz instantâneo
+                    #Hertz instantâneo Suavizado -> menos suscetível a grandes variações. Isso é importante pois a taxa de hz sofre microvariações em decorrência dos tipos dedados envolvidos
+                    #e à própria variação da precisão computacional. A taxa inst é calculada para fins didáticos e será atualizada poucas vezes por segundo, já que a Hz altos se torna impossível de ler
                     #####################################
-                    agora = time.perf_counter()
+
                     dt_inst = agora - self.t_anterior
 
                     self.t_anterior = agora
 
                     if dt_inst>0:
 
-                        hz = 1/dt_inst
+                        self.hz_inst  = 1/dt_inst #hz inst real
+                        
 
-                        self.hzs.append(hz)
-                        self.hz_inst = sum(self.hzs) / len(self.hzs)
+                        self.deque_hzs_inst_suavizado.append(self.hz_inst)
+                        self.hz_inst_suavizado = sum(self.historico_hzs_inst) / len(self.historico_hzs_inst)
 
 
+                    ####################################
+                    #Hz instantâneo
+                    ####################################
+                    #agora = time.perf_counter()
+                    #dt_inst = agora - self.t_anterior
 
+                    '''self.t_anterior = agora
+
+                    if dt_inst>0:
+
+                        hz = 1/dt_inst'''
 
                     #print("Recebi uma amostra")
 
